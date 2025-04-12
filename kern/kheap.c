d8f9c06feb2fc4fd695f561b564ed614448cd44b
MohamedBadra <madobadra3@gmail.com>
2025-04-13 01:03:42 +0200

    kmalloc

--- kern/kheap.c
+++ kern/kheap.c
@@ -9,13 +9,50 @@ void* kmalloc(unsigned int size)
 {
 	//TODO: [PROJECT 2025 - MS1 - [1] Kernel Heap] kmalloc()
 	// Write your code here, remove the panic and write your code
-	kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+	//kpanic_into_prompt("kmalloc() is not implemented yet...!!");
 
 	//NOTE: Allocation is based on FIRST FIT strategy
 	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
 	//refer to the project presentation and documentation for details
 
 	//change this "return" according to your answer
+	uint32 i=nextFit;
+	uint32 start=-1;
+
+	do{
+		if(start !=-1 && i-start==ROUND_UP(size,PAGE_SIZE)/PAGE_SIZE)
+				{
+
+					struct Frame_Info* fptr=NULL;
+					for(uint32 j=start;j<i;j=(j+1)%KHEAP_ARR_SIZE)
+					{
+						allocate_frame(&fptr);
+						if(fptr==NULL)
+						{ cprintf("\n no frame found for page, memory is FULL\n");
+							return NULL;
+						}
+						int res=map_frame(ptr_page_directory,fptr,(j*PAGE_SIZE)+KERNEL_HEAP_START);
+						if(res!=0)
+						{ 	cprintf("\n no frame found for page table, memory is FULL\n");
+							return NULL;
+						}
+					}
+					KHEAP_ARR[start]=i-start;
+					nextFit=i;
+					return (start*PAGE_SIZE)+KERNEL_HEAP_START;
+				}
+				else if(KHEAP_ARR[i]==-1 && start==-1) start=i;
+				else if(KHEAP_ARR[i]!=-1)
+				{
+					i=(i+KHEAP_ARR[i])%KHEAP_ARR_SIZE;
+					start=-1;
+					continue;
+				}
+				i=(i+1);
+				if(i==KHEAP_ARR_SIZE) {start=-1; i=0;}
+	}while(i != nextFit);
+
+
 	return NULL;
 }
 
@@ -27,7 +64,7 @@ void kfree(void* virtual_address)
 
 	//you need to get the size of the given allocation using its address
 	//refer to the project presentation and documentation for details
-	
+
 
 }
 
@@ -50,14 +87,14 @@ unsigned int kheap_virtual_address(unsigned int physical_address)
 		if(ptr != NULL){
 			pa = to_physical_address(ptr);
 			if((pa >> 12) == (physical_address >> 12)){
-				va += (physical_address & 4059);
+				va += (physical_address & 4095);
 				return va;
 			}
 		}
-		va += 4096;
+		va += PAGE_SIZE;
 	}
-	
-	
+
+
 	return -1;
 }
 
@@ -71,18 +108,18 @@ unsigned int kheap_physical_address(unsigned int virtual_address)
 	//refer to the project presentation and documentation for details
 
 	//change this "return" according to your answer
-		
+
 	uint32* page_table_ptr = NULL;
-		
+
 	get_page_table(ptr_page_directory, (void *)virtual_address, 0, &page_table_ptr);
 	if(page_table_ptr == NULL){
 		return -1;
 	}
 	uint32 pte_ptr = page_table_ptr[PTX(virtual_address)];
 	uint32 frameNo = pte_ptr >> 12;
-	
 
-	return frameNo * 4096;
+
+	return frameNo * PAGE_SIZE;
 }
 
 void *krealloc(void *virtual_address, uint32 new_size)
